058d99955ad8f54fb45a32dc631358bf9de8af14
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-07-03 10:22:17 +0530

    chore(ts): ignore build artifacts and node_modules

--- ts/dist/defensive/permission.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.permissionReminder = permissionReminder;
-/**
- * Consistent permission reminder for defensive cybersecurity tooling.
- */
-function permissionReminder() {
-    return 'Use only on systems you own or have explicit permission to assess.';
-}
